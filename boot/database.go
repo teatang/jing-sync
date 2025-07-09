ee977b5746908d8690dc95bac24e21bfb6361f94
TeaTang <tea.tang.1@lexisnexis.com>
2025-07-09 17:05:04 +0800

    db name

--- boot/database.go
+++ boot/database.go
@@ -1,21 +1,22 @@
 package boot
 
 import (
+	"jing-sync/models"
+	"jing-sync/utils"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"jing-sync/utils"
-	"jing-sync/models"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	if !utils.FileExists("data/user.db") {
-	    DB, err = gorm.Open(sqlite.Open("data/user.db"), &gorm.Config{})
+	if !utils.FileExists("data/jing-sync.db") {
+		DB, err = gorm.Open(sqlite.Open("data/jing-sync.db"), &gorm.Config{})
 		DB.AutoMigrate(&models.User{})
 	} else {
-		DB, err = gorm.Open(sqlite.Open("data/user.db"), &gorm.Config{})
+		DB, err = gorm.Open(sqlite.Open("data/jing-sync.db"), &gorm.Config{})
 	}
 	if err != nil {
 		panic("failed to connect database")
